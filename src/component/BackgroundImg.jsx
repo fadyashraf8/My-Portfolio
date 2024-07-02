b2eddeb88e6156d67fc56be1798682379384b8b5
fadyashraf8 <fadyashraf909@gmail.com>
2024-07-02 06:33:32 +0300

    test

--- src/component/BackgroundImg.jsx
+++ src/component/BackgroundImg.jsx
@@ -14,7 +14,7 @@ const BackgroundImg = () => {
                     <p>HI, I'M A FREELANCER</p>
                     <h1>REACT.JS-NODE.JS-FULL STACK DEVELOPER</h1>
                     <div className=''> 
-                        <a href='https://drive.google.com/file/d/1tvMwsdqNnnCVe9K99xtHPL9QD5wVgYtE/view?usp=drive_link' target='_blank' className='button m-3'>
+                        <a href='https://drive.google.com/file/d/1tvMwsdqNnnCVe9K99xtHPL9QD5wVgYtE/view?usp=drive_link' target='_blank' className='button m-3 sora w-50'>
                             My Resume
                         </a>
                         <Link to='/contact' className='button button-light'>
